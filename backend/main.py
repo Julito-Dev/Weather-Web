abc88bb321bc9f771cf58d1f55d67aa101a7a701
Julito-Dev <julianpaez180@gmail.com>
2026-07-24 12:16:41 -0500

    feat: Home endpoint

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from services.weather_service import WheatherService
 
 
 app = FastAPI()
 
+templates = Jinja2Templates(directory="../templates")
+app.mount("/static", StaticFiles(directory="../static"), name="static")
+
+service = WheatherService()
 
 @app.get("/")
-async def main():
-    return f"Hello"
+def home(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="index.HTML"
+    )
 
+@app.get("/weather")
+async def weather(city: str):
+    return service.search(city)
