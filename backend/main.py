c2b75a2c53e7e4e7d98e6a9b60e6b3b39338b293
Julito-Dev <julianpaez180@gmail.com>
2026-07-24 12:53:38 -0500

    feat: random forecast generator endpoint added

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,7 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from services.weather_service import WheatherService
-
+import random
 
 app = FastAPI()
 
@@ -11,6 +11,10 @@ templates = Jinja2Templates(directory="../templates")
 app.mount("/static", StaticFiles(directory="../static"), name="static")
 
 service = WheatherService()
+
+
+
+CITIES = ["Bogota", "Buenos Aires", "London", "Paris", "Madrid", "Sydney", "Washington", "New York", "Lima", "Sao Paulo", "Monaco", "Cairo", "Santiago", "Tokyo"]
 
 @app.get("/")
 def home(request: Request):
@@ -22,3 +26,16 @@ def home(request: Request):
 @app.get("/weather")
 async def weather(city: str):
     return service.search(city)
+
+
+@app.get("/feature")
+async def randomSearch():
+    cities = random.sample(CITIES, 4)
+    
+    weather_cities = []
+    
+    for city in cities:
+        weather_cities.append(service.search(city))
+        
+    return weather_cities
+
